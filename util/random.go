67a496efc89f330619c2eece07b2aaf6843e26a1
Young wook Jeon <ela87@naver.com>
2023-04-23 16:55:10 +0900

    Add a custom validator for checking currency

--- util/random.go
+++ util/random.go
@@ -37,7 +37,7 @@ func RandomMoney() int64 {
 }
 
 func RandomCurrency() string {
-	currencies := []string{"USD", "EUR", "KRW"}
+	currencies := []string{EUR, USD, KRW}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
